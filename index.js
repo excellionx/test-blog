1d5b812ee1a2111f78087cf5338382d73a3277e1
Kevin <kevindizoncua@gmail.com>
2018-06-01 19:32:11 -0700

    Hello

---
+++ index.js
@@ -0,0 +1,49 @@
+'
+use strict';
+
+var express = require('express'),
+ bodyParser = require('body-parser');
+ Realm = require('realm');
+
+var app = express();
+
+let postSchema = {
+	name: 'Post',
+	properties: {
+		timestamp: 'date',
+		title: 'string',
+		content: 'string'
+	}
+};
+
+var blogRealm = new Realm({
+	path: 'blog.realm',
+	schema: [PostSchema]
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+app.post('/write', function(req, res) {
+	let title = req.body['title'],
+	content = req.body['content'],
+	timestamp = new Date();
+	blogRealm.write(() => {
+	 blogRealnm.create('Post', {title: title, content: content, timestamp: timestamp});
+	});
+	res.sendFile(__dirName + "/write-complete.html");
+});
+
+app.get('/', function(req, res){
+	let posts = blogRealm.objects('Post').sorted('timestamp', true);
+	res.render('index.ejs', {posts: posts});
+});
+
+app.listen (3000, function(){
+	console.log("Go!");
+});
+
+app.get('/write', function(req, res){
+	res.sendFile(__dirname + "/write.html");
+});
+
